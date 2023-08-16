d7193074327c655d1232dac8d0c32c29a0759c8a
motypes <mohammed.jalil@gmail.com>
2023-08-16 16:28:29 +0800

    implemented dark, light and system theme

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,6 +6,7 @@ import { UserButton } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const font = Poppins({
   weight: "600",
@@ -33,6 +34,7 @@ const NavBar = () => {
           Upgrade
           <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
         </Button>
+        <ModeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
