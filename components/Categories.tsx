258c0f4e91eab7d5ac74e27fdba8b2a131050d6f
motypes <mohammed.jalil@gmail.com>
2023-08-21 14:47:03 +0800

    categories completed

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,15 +2,38 @@
 
 import { cn } from "@/lib/utils";
 import { Category } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 interface CategoriesProps {
   data: Category[];
 }
 
 const Categories = ({ data }: CategoriesProps) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const categoryId = searchParams.get("categoryId");
+
+    const onClick = (id: string | undefined) => {
+        const query = { categoryId: id };
+        const url = qs.stringifyUrl(
+            {
+                url: window.location.href,
+                query: query,
+            },
+            {
+                skipNull: true,
+                skipEmptyString: true,
+            }
+        );
+
+        router.push(url);
+    };
+
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
       <button
+        onClick={() => onClick(undefined)}
         className={cn(`
             flex
             items-center
@@ -25,13 +48,15 @@ const Categories = ({ data }: CategoriesProps) => {
             bg-primary/10
             hover:opacity-75
             transition
-            `)}
+            `, 
+            !categoryId ? "bg-primary/25" : "bg-primary/10")}
       >
         Newest
       </button>
       {data.map((item) => {
         return (
           <button
+            onClick={() => onClick(item.id)}
             key={item.id}
             className={cn(`
                         flex
@@ -47,7 +72,7 @@ const Categories = ({ data }: CategoriesProps) => {
                         bg-primary/10
                         hover:opacity-75
                         transition
-                        `)}
+                        `, item.id === categoryId ? "bg-primary/25" : "bg-primary/10")}
           >
             {item.name}
           </button>
