47b2bf8448a23d15df58e7ae1aebec42e6cbe6ac
motypes <mohammed.jalil@gmail.com>
2023-08-27 17:07:24 +0800

    fixed mobilesidebar

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -3,14 +3,20 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import Sidebar from "@/components/Sidebar";
 
-const MobileSidebar = () => {
+interface MobileSidebarProps {
+  isPro: boolean;
+}
+
+const MobileSidebar = (
+  { isPro }: MobileSidebarProps
+) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro}/>
       </SheetContent>
     </Sheet>
   );
